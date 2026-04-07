ce721bc783535d04d2e4e1812a9ce0da4dcc5574
fabiancella <fabiancella@gmail.com>
2026-04-07 15:02:02 -0400

    fix category index issue

--- opensky_feeder.py
+++ opensky_feeder.py
@@ -27,7 +27,7 @@ for state in data["states"]:
         "altitude_ft" : state[7] * 3.281,
         "groundspeed_kt" : state[9] * 1.944,  
         "heading" : state[10],
-        "category": state[17]
+        "category": state[17] if len(state) > 17 else None
     }
     
     response = requests.post(API_URL, json=payload)
